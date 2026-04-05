e83f7b8b522cdaed76b15a007a32f7e98d3b6478
AtaS <atasasmaz@gmail.com>
2026-04-05 14:29:55 +0100

    standardiation refactoring, add stored procedure

--- db/init.sql
+++ db/init.sql
@@ -16,3 +16,18 @@ SELECT create_hypertable('prices', 'time', if_not_exists => TRUE);
 CREATE UNIQUE INDEX IF NOT EXISTS idx_prices_symbol_time_gran
     ON prices (symbol, time, granularity);
 
+CREATE OR REPLACE FUNCTION get_symbol_prices(
+    p_symbol TEXT,
+    p_interval INTERVAL,
+    p_from TIMESTAMPTZ,
+    p_to TIMESTAMPTZ
+) RETURNS TABLE("time" TIMESTAMPTZ, value DOUBLE PRECISION) AS $$
+    SELECT time_bucket(p_interval, t.time) AS time, avg(t.close) AS value
+    FROM prices t
+    WHERE t.time BETWEEN p_from AND p_to
+      AND t.symbol = p_symbol
+      AND (t.granularity = 'daily' OR t.time >= CURRENT_DATE)
+    GROUP BY 1
+    ORDER BY 1;
+$$ LANGUAGE sql STABLE;
+
